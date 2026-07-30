8e09cc922e726dcf342c22f40c994d22d8fd8b7d
Corvo Socidigital <211055848+Gabriel-g1t@users.noreply.github.com>
2026-07-30 12:29:02 -0300

    .

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementByIdById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // Boa Tarde!
+        img.src = "fotomanha.jpg"
+    } else if (hora >= 12 && hora < 18){
+        // Boa Tarde!
+        img.src = "fototarde.jpg"
+    } else {
+        // Boa Noite!
+        img.src = "fotonoite.jpg"
+    }
+}
