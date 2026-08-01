32cefa372cedbc4d73950b60e588ea8045ba9347
Corvo Socidigital <211055848+Gabriel-g1t@users.noreply.github.com>
2026-08-01 14:14:50 -0300

    exercicio 2

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,17 +1,21 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementByIdById('imagem')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    // var hora = 21
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        // Boa Tarde!
-        img.src = "fotomanha.jpg"
+        // Boa Dia!
+        img.src = 'imagens/fotomanha.jpg'
+        document.body.style.background = '#e6ce9b'
     } else if (hora >= 12 && hora < 18){
         // Boa Tarde!
-        img.src = "fototarde.jpg"
+        img.src = 'imagens/fototarde.jpg'
+        document.body.style.background = '#dc9b0d'
     } else {
         // Boa Noite!
-        img.src = "fotonoite.jpg"
+        img.src = 'imagens/fotonoite.jpg'
+        document.body.style.background = '#02173c'
     }
 }
