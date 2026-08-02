c3bcc3d2281251c853e4d8135490d134fbc7b7d0
Corvo Socidigital <211055848+Gabriel-g1t@users.noreply.github.com>
2026-08-02 12:47:09 -0300

    imagens

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,11 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('itxtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.lenght == 0 || fano.value > ano {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        window.alert('Tudo ok')
+    }
+}
