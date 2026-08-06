d6647539d94ed687b5e26501bd5c35b13069c337
Corvo Socidigital <211055848+Gabriel-g1t@users.noreply.github.com>
2026-08-06 18:11:29 -0300

    desafio ex16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function contar() {
+    var inicio = Number(document.getElementById('c1').value)
+    var fim = Number(document.getElementById('c2').value)
+    var passo = Number(document.getElementById('p').value)
+    var res = window.document.querySelector('div#res')
+
+    res.innerHTML = ''
+
+    if (inicio == 0 || fim == 0 || passo == 0) {
+        res.innerHTML= 'Impossivel contar!'
+    } else if (passo <= 0) {
+        alert('[ERRO] Passo inválido! Considerando PASSO 1')
+        passo = 1
+    } else {
+        while (inicio <= fim) {
+        res.innerHTML += inicio
+        inicio += passo
+        }
+    }
+}
