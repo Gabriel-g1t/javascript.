1d75de535b71ccb65c89e65335513ff147223507
Corvo Socidigital <211055848+Gabriel-g1t@users.noreply.github.com>
2026-08-08 14:33:48 -0300

    ex17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Olá')
+function gerar() {
+    let num = window.document.getElementById('inumero')
+    let res = window.document.querySelector('div#res')
+
+    if (num.value.length == "") {
+        alert('[ERRO] Você não digitou nenhum número')
+    } else {
+        let n = Number(num.value)
+        
+        res.innerHTML = ''
+        
+        for (let g = 1; g <= 10; g++) {
+                res.innerHTML += `${n} x ${g} = ${n * g}<br>`
+         }
+    }
+}
