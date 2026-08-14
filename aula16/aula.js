ba68b7b49399bdcee690aa33cba9d4bcc01917cf
Corvo Socidigital <211055848+Gabriel-g1t@users.noreply.github.com>
2026-08-14 17:42:49 -0300

    FUNÇÕES

---
+++ aula16/aula.js
@@ -0,0 +1,14 @@
+function parimpar(n) {
+    if (n%2==0) {
+        console.log('par')
+    } else {
+        console.log('ímpar')
+    }
+}
+
+/*
+let res = parimpar(5)
+console.log(res) */
+// com variavel
+
+console.log(parimpar(220))
