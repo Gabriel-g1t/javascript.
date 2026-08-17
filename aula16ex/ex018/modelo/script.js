381947eedfdf9493b655ff4b594eb87df6b70204
Corvo Socidigital <211055848+Gabriel-g1t@users.noreply.github.com>
2026-08-17 19:25:06 -0300

    2 dia tentando exercicio

--- aula16ex/ex018/modelo/script.js
+++ aula16ex/ex018/modelo/script.js
@@ -1,11 +1,21 @@
 function adicionar() {
     let num = window.document.getElementById('inum')
     let li = document.getElementById('ilista')
-    let res = document.querySelector('div#res')
 
     num = Number(num.value)
-    if (num <= 0) {
-        alert('NAO')
+    if (num <= 0 || num > 100) {
+        alert('[ERRO] Insira um número válido')
+    } else {
+            let item = document.createElement('option')
+            item.text = `Valor ${num} adicionado.`
+            li.appendChild(item)
     }
 
+    function verificar() {
+    let res = document.querySelector('div#res')
+
+    if (num.length > 0){
+        res.innerHTML = `Ao todo, temos ${num.length} números cadastrados.`
+    }
+}
 }
