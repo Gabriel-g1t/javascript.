a66199f7618b97c6f87d0154cd028c916ed68473
Corvo Socidigital <211055848+Gabriel-g1t@users.noreply.github.com>
2026-08-18 19:53:22 -0300

    não consegui finalizar hoje

--- aula16ex/ex018/modelo/script.js
+++ aula16ex/ex018/modelo/script.js
@@ -10,12 +10,17 @@ function adicionar() {
             item.text = `Valor ${num} adicionado.`
             li.appendChild(item)
     }
+}
 
     function verificar() {
     let res = document.querySelector('div#res')
+    let lista = document.getElementById('ilista')
+    let maior = lista.options[0]
 
-    if (num.length > 0){
-        res.innerHTML = `Ao todo, temos ${num.length} números cadastrados.`
+
+    if (lista.options.length > 0) {
+        res.innerHTML = `<p> Ao todo, temos ${lista.options.length} números cadastrados. </p>`
+    } for (let x = 0; x < lista.options.length; x++) {
+        res.innerHTML += `O maior valor informado é ${x.options.length}`
     }
-}
 }
